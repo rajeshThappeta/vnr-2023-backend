5617fd0327e977a2bed638042c573bb2e9453739
rajesh <rajesh.t7982@gmail.com>
2024-02-22 20:01:28 +0530

    user registration& login

--- APIs/userApi.js
+++ APIs/userApi.js
@@ -1,82 +1,139 @@
 //create min-express app
-const exp=require('express')
+const exp=require('express');
 const userApp=exp.Router()
+
+const bcryptjs=require('bcryptjs')
+
+const jwt=require('jsonwebtoken')
+
 
 
 //body parser
 userApp.use(exp.json());
 
-//sample users data
-let users = [
-  {
-    id: 1,
-    name: "kiran",
-  },
-  {
-    id: 2,
-    name: "ravi",
-  },
-];
+
 //user api(REST API)
 
 //get all users
-userApp.get("/users", (req, res) => {
-  //send users in res
-  res.send({ message: "all users", payload: users });
+userApp.get("/users", async(req, res) => {
+
+    //get usersCollectionObject from express obj
+   const usersCollectionObject= req.app.get('usersCollectionObject')
+    //GET USERS DOCUMENTS FROM DB
+   let usersList= await usersCollectionObject.find().toArray()
+   
+    //SEND RES
+    res.send({message:"all users",payload:usersList})
 });
 
-//get a user by id
-userApp.get("/users/:id", (req, res) => {
-  //get url param
-  let id = Number(req.params.id); //{ id : 2}
-  //find user by id
-  let user = users.find((userObj) => userObj.id === id);
-  //send res
-  if (user === undefined) {
-    res.send({ message: "No user found" });
-  } else {
-    res.send({ message: "a user", payload: user });
-  }
+
+
+//get a user by username
+userApp.get("/users/:username", async(req, res) => {
+     //get usersCollectionObj from express app
+     const usersCollectionObject= req.app.get('usersCollectionObject')
+     //get username from url param
+     let usernameFromUrl=req.params.username
+     //secrh in db for user
+     let userFromDb=await usersCollectionObject.findOne({username:usernameFromUrl})
+     //send res
+     res.send({message:"user",payload:userFromDb})
+  
 });
+
+
+
 
 //create a user
-userApp.post("/new-user", (req, res) => {
-  //get new user obj
-  let newUser = req.body;
-  //insert newUser in users array
-  users.push(newUser);
-  //send res
-  res.send({ message: "New user created" });
+userApp.post("/new-user", async(req, res) => {
+    //get usersCollectionObj from express app
+    const usersCollectionObject= req.app.get('usersCollectionObject')
+    //get user from req
+    let newUser=req.body;
+    //check duplicate user by username
+    let dbuser=await usersCollectionObject.findOne({username:newUser.username})
+    //if user found
+    if(dbuser!==null){
+        res.send({message:"User already existed"})
+    }else{
+    //hash password
+     let hashedPassword=   await bcryptjs.hash(newUser.password,6)
+     //replace plain password with hashed pw
+     newUser.password=hashedPassword;
+    //save user in db
+    await usersCollectionObject.insertOne(newUser)
+    //send res to client
+    res.send({message:"New User created"})
+    }
+    
 });
 
-//update a user by id
-userApp.put("/user", (req, res) => {
-  //get modifieduser details
-  let modifiedUser = req.body;
-  //find index of user need to be modified
-  let index = users.findIndex((userObj) => userObj.id === modifiedUser.id);
-  //if not user found
-  if (index === -1) {
-    res.send({ message: "No user found to update" });
-  } else {
-    users.splice(index, 1, modifiedUser);
-    res.send({ message: "User modified" });
-  }
+
+//user login route
+userApp.post('/login',async(req,res)=>{
+
+    //get user coll obj
+    const usersCollectionObject= req.app.get('usersCollectionObject')
+    //get user cred obj from client
+    const userCredObj=req.body;
+    //verify username
+    let dbuser=await usersCollectionObject.findOne({username:userCredObj.username})
+    //if user not found
+    if(dbuser===null){
+        res.send({message:"Invalid username"})
+    }else{
+         //verify password
+        let status=await bcryptjs.compare(userCredObj.password,dbuser.password)
+        //if passwords arev not matched
+        if(status===false){
+            res.send({message:"Invalid password"})
+        }else{
+            //create JWT token
+        let encodedToken=  jwt.sign({username:dbuser.username},'abcdef',{expiresIn:20})
+            //send token to client
+            res.send({message:"login success",token:encodedToken,user:dbuser})
+        }
+    }
+   
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+//update a user by username
+userApp.put("/user", async(req, res) => {
+     //get usersCollectionObj from express app
+     const usersCollectionObject= req.app.get('usersCollectionObject')
+     //get mnodified resource from client
+     let modifiedUser=req.body;
+     //uopdate in db
+    await usersCollectionObject.updateOne({username:modifiedUser.username},{$set:{...modifiedUser}})
+    //send res
+    res.send({message:"User modified"})
 });
 
 //delete a user by id
-userApp.delete("/users/:id", (req, res) => {
-  //get id from url
-  let id = Number(req.params.id);
-  //find index of user to be deleted
-  let index = users.findIndex((userObj) => userObj.id === id);
-  //if not user found
-  if (index === -1) {
-    res.send({ message: "No user found to delete" });
-  } else {
-    users.splice(index, 1);
-    res.send({ message: "User deleted" });
-  }
+userApp.delete("/users/:username", async(req, res) => {
+    //get usersCollectionObj from express app
+    const usersCollectionObject= req.app.get('usersCollectionObject')
+     //get username from url param
+     let usernameFromUrl=req.params.username
+     //delete user by username from db
+    let dbres= await usersCollectionObject.deleteOne({username:usernameFromUrl})
+    
+     //send res
+     if(dbres.deletedCount===1){
+     res.send({message:"User removed"})
+     }
 });
 
 
--- server.js
+++ server.js
@@ -4,6 +4,28 @@ const exp = require("express");
 //2.create express app
 const app = exp();
 
+//import MongoClient
+const mongoClient=require('mongodb').MongoClient
+//connect mongodb server using mongoClient
+mongoClient.connect('mongodb://localhost:27017')
+.then(client=>{
+
+  //get database object
+    const dbObj=client.db('vnrb2db')
+  //get collection object
+  const usersCollectionObject=dbObj.collection('userscollection')
+  const productsCollectionObject=dbObj.collection('productscollection')
+  //share two collection objects to APIs
+  app.set('usersCollectionObject',usersCollectionObject)
+  app.set('productsCollectionObject',productsCollectionObject)
+  //confiramtion of DB connction
+  console.log("DB connection success")
+})
+.catch(err=>{
+  console.log("DB coonection error",err)
+})
+
+
 //import userApp and productApp
 const userApp=require('./APIs/userApi')
 const productApp=require('./APIs/productsApi')
@@ -12,6 +34,7 @@ const productApp=require('./APIs/productsApi')
 app.use('/user-api',userApp)
 //forward req to productApp if path starts with '/product-api'
 app.use('/product-api',productApp)
+
 
 //error handling middleware
 app.use((err, req, res, next) => {
